372411ffaca3f0975882aa3a8dd21d39fa84ffba
Ent <heech.kang@gmail.com>
2025-11-19 10:50:06 +0900

    feat: first commit

---
+++ assistantTests/assistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  assistantTests.swift
+//  assistantTests
+//
+//  Created by Ent on 11/18/25.
+//
+
+import Testing
+@testable import assistant
+
+struct assistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
